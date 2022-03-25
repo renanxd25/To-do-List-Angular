3537bd52865cf44db34b7de8ccea7e4cda0fa872
Renan Mendes <renanxd7x@hotmail.com>
2022-03-25 17:55:09 -0300

    adicionando o local storage no projeto

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 //interface
 import { TaskList } from '../../model/task-list';
@@ -8,14 +8,17 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
-  public taskList: Array<TaskList> =[];
+  public taskList: Array<TaskList> =JSON.parse(localStorage.getItem("list")||'[]');
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngDoCheck(): void {
+      this.setLocalStorate();
+    
   }
+
   public setEmitTaskList(event: string){
     this.taskList.push({task: event, checked:false})
   }
@@ -30,6 +33,22 @@ export class TodoListComponent implements OnInit {
     if(confirm){
       this.taskList = [];
     }
+  }
+
+  public validationInput(event: string, index: number){
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja Deletar?");
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
+    }
+  }
+  public setLocalStorate(){
+    if(this.taskList){
+      this.taskList.sort((first, last)=> Number(first.checked)-Number(last.checked));
+      localStorage.setItem("list",JSON.stringify(this.taskList))
+    }
+
   }
 
 }
